f0ca0c9cec6bebad94677d77b63971e3938a57a6
TannerF21 <freitagj5046@uhcl.edu>
2025-06-23 15:13:05 -0500

    Initial Game 1 round

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
   _______
 ---'   ____)
@@ -24,3 +26,27 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+choices = [rock, paper, scissors]
+
+user_choice= int(input("Type 0 for Rock, 1 for Paper, or 2 for Scissors"))
+
+if user_choice < 0 or user_choice > 2:
+    print("Invalid Choice!")
+else:
+    computer_choice = random.randint(0,2)
+
+    print("\nYou chose: ")
+    print(choices[user_choice])
+
+    print("Computer chose: ")
+    print(choices[computer_choice])
+
+    if user_choice == computer_choice:
+        print("It's a draw!")
+    elif(user_choice == 0 and computer_choice == 2) or \
+        (user_choice == 1 and computer_choice == 0) or \
+        (user_choice == 2 and computer_choice == 1):
+        print("You Win!")
+    else:
+        print("You Lose!")
